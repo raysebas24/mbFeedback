682589c8dd3d2dccb2e7a8d68f19799d78967730
raysebas24 <ray.sebastian100@gmail.com>
2023-03-15 22:04:31 +0100

    change PassWd and EmailAddress

--- send_mail.py
+++ send_mail.py
@@ -3,13 +3,14 @@ from email.mime.text import MIMEText
 
 def send_mail(customer, dealer, rating, comments):
     port = 587
+    emailName = '???' # default DragonBall Mail Adress
     smtp_server = 'smtp.gmail.com'
-    login = 'son.goku93288@gmail.com'
-    password = 'hrvy hhms ztxv jgqw'
+    login = emailName
+    password = '???' # default DragonBall Mail PassWD
     message = f"<h3>New Feedback Submission</h3><ul><li>Customer: {customer}</li><li>Dealer: {dealer}</li><li>Rating: {rating}</li><li>Comments: {comments}</li></ul>"
 
-    sender_email = 'son.goku93288@gmail.com'
-    receiver_email = 'son.goku93288@gmail.com'
+    sender_email = emailName
+    receiver_email = emailName
     msg = MIMEText(message, 'html')
     msg['Subject'] = 'Mercedes Feedback'
     msg['From'] = sender_email
